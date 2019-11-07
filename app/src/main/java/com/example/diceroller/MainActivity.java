362458102c7755003455c1ebab58fb9c9e6d01d8
Mohammed Jaman Islam <56393712+mjislam18@users.noreply.github.com>
2019-11-07 11:40:54 +0000

    Updated program to make the extensible

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -11,20 +11,30 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> Message = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Message.add("If you could go any where in the world where would you go?");
+        Message.add("If you were stranded on a desert island, what three things would you want to take with you?");
+        Message.add("If you could eat only one food for the rest of your life what would it be?");
+        Message.add("If you won a million dollars what is the first thing you would buy?");
+        Message.add("If you could spend the day with one fictional character who would it be?");
+        Message.add("If you found a magic lantern and a genie gave you three wishes,what would you wish?");
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,24 +75,25 @@ public class MainActivity extends AppCompatActivity {
         roll_the_dice();
 
         TextView TV = this.findViewById(R.id.NumTextView);
-        TextView EG = (TextView) this.findViewById(R.id.EnterGuess);
-        TextView SC = (TextView) this.findViewById(R.id.ScoreText);
+        TextView EG = this.findViewById(R.id.EnterGuess);
+        TextView SC = this.findViewById(R.id.ScoreText);
 
         int Number = Integer.valueOf(TV.getText().toString());
         int UserGuess = Integer.valueOf(EG.getText().toString());
 
         if (UserGuess == Number) {
 
-        int Score = Integer.valueOf(SC.getText().toString());
-        int ScoreCheck = Score +1;
+            int Score = Integer.valueOf(SC.getText().toString());
+            int ScoreCheck = Score + 1;
 
-        SC.setText(Integer.toString(ScoreCheck));
-        Toast.makeText(getApplicationContext(), "Congratulations", Toast.LENGTH_SHORT).show();
+            SC.setText(Integer.toString(ScoreCheck));
+            Toast.makeText(getApplicationContext(), "Congratulations", Toast.LENGTH_SHORT).show();
 
         }
-        }
+    }
 
     public void on_button_Click2(View view) {
+
         roll_the_dice();
 
         TextView TV = this.findViewById(R.id.NumTextView);
@@ -90,23 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
         int Number = Integer.valueOf(TV.getText().toString());
 
-        if (Number == 1){
-            MS.setText("If you could go any where in the world where would you go?");
-        }else if (Number == 2){
-            MS.setText("If you were stranded on a desert island, what three things would you want to take with you?");
-        }
-        else if (Number == 3){
-            MS.setText("If you could eat only one food for the rest of your life what would it be?");
-        }
-        else if (Number == 4){
-            MS.setText("If you won a million dollars what is the first thing you would buy?");
-        }
-        else if (Number == 5){
-            MS.setText("If you could spend the day with one fictional character who would it be?");
-        }
-        else if (Number == 6){
-            MS.setText("If you found a magic lantern and a genie gave you three wishes,what would you wish?");
-        }
+        MS.setText(Message.get(Number-1));
 
     }
 
